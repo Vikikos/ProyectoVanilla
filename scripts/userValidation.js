fe8f337c75fa6b41df231fb23894233a4924d9fd
Vikikos <viktoriakosarova272@gmail.com>
2025-11-11 16:53:41 +0100

    casa-2

--- scripts/userValidation.js
+++ scripts/userValidation.js
@@ -1,34 +1,70 @@
+    // localStorage.removeItem('userData2');
+    // localStorage.removeItem('usersCont');
+    // localStorage.removeItem('sessionId');
 document.getElementById('userName').addEventListener('blur',function (){
-    if(this.value){
+    var value = this.value;
+    if(value){
         //si hay texto en input
-        let userCont = localStorage.getItem('usersCont');
+        var userCont = parseInt( localStorage.getItem('usersCont'));
         if(userCont){
+            let noExists = false;
             //si hay usuarios
-            for (let i = 1; i < userCont; i++) {
+            for (let i = 1; i <= userCont; i++) {
                 //recoremos todos los usuarios existentes
-                let idUser = 'userData' + i;
+                let idUser = 'userData' + i;//nombre de la variable
+
+                //obtenemos los datos de ese usuario
                 const savedUserData = JSON.parse(localStorage.getItem(idUser));
-                
+
+                if(savedUserData['username'] === value){
+                    //si ya existe 
+                    localStorage.setItem('sessionId', savedUserData['id']);
+                    noExists = false;
+                    break;
+                }else{
+                    noExists = true;
+                }
+                //console.log(savedUserData['username']);
             }
+            if(noExists){
+                //si no existe el usuario lo creamos
+                let idUser = 'userData' + (userCont+1);
+                let userData = {
+                    id: (userCont+1),
+                    username: value,
+                    day:'',
+                    time:'',
+                };
+                //creamos al usuario
+                localStorage.setItem(idUser, JSON.stringify(userData));
+                //establecemos que el usuario actual con el id
+                localStorage.setItem('sessionId', (userCont+1));
+                //sumamos el count de usuarios
+                localStorage.setItem('usersCont',(userCont+1));
+                //console.log('sesion iniciada en usuario nuevo')
+            }
+            //console.log('sesion iniciada en usuario existente')
         }else{
             //si no hay ningun usuario
 
             //iniciamos la variable contador de usuarios
             localStorage.setItem('usersCont',1);
             
-            //creamos usuario
-            const userData = {
+            //creamos usuario con id 1 por ser el primero
+            let userData = {
                 id: 1,
-                username: "john_doe",
+                username: value,
                 day:'',
                 time:'',
             };
+            //creamos al usuario
+            localStorage.setItem("userData1", JSON.stringify(userData));
+            //establecemos que el usuario actual con el id
+            localStorage.setItem('sessionId', 1);
         }
-        //window.location.href = '/pantalla2.html';
-        console.log(this.value);
+        //siempre redirige
+        window.location.href = '/pantalla2.html';
 
-        const savedUserData = JSON.parse(localStorage.getItem("userData"));
-        console.log(savedUserData['username']);
     }else{
         //si no hay texto en input
         console.log('vacio')
