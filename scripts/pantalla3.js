a13d7aa7057b737e5ece919da6659e2efc96f384
Vikikos <viktoriakosarova272@gmail.com>
2025-11-12 12:09:26 +0100

    casa-4

---
+++ scripts/pantalla3.js
@@ -0,0 +1,54 @@
+function getCuestions(delay = false) {
+    console.log('si entra')
+    let delayTime = 0;
+    if(delay){
+        delayTime = 5000;
+    }
+    setTimeout(() => {
+        //obtenemos al user actual
+        let sessionId = localStorage.getItem('sessionId');
+        let contQuest = localStorage.getItem('contQuest');
+        if(contQuest){
+            //hay preg8untas
+            let questions = [];
+            for (let i = 1; i <= contQuest; i++) {
+                //recorremos todas las preguntas
+                const savedQuest = JSON.parse(localStorage.getItem(q+i));
+                if(savedQuest['userid'] == sessionId){
+                    //la pregunta es del usuario
+                    questions.push(savedQuest);
+                }
+            }
+            if(questions.length == 0){
+                //no hay preguntas del usuario
+                document.getElementById('noQuest').innerHTML = '<td colspan="4">No hay preguntas</td>';
+            }else{
+                //hay preguntas del usuario
+                let cont = 1;
+                questions.forEach(question => {
+                    let text = '<tr>';
+                    text += '<td>Pregunta'+ cont +'</td>';
+                    text += '<td>'+ question['question'] +'</td>';
+                    text += '<td>'+ question['answer'] +'</td>';
+                    text += '<td>'+ question['score'] +'</td>';
+                    text += '<td>OK</td>';
+                    text += '</tr>';
+                });
+            }
+        }else{
+            //no hay preguntas
+            document.getElementById('noQuest').innerHTML = '<td colspan="4">No hay preguntas</td>';
+        }
+    }, delayTime);
+}
+
+window.addEventListener('load', getCuestions());
+
+
+
+// let q + id  = {
+//             userid: sessionid,
+//             question: string,
+//             answer:frue o false,
+//             score:0-9,
+//         };
--- scripts/userValidation.js
+++ scripts/userValidation.js
@@ -1,80 +1,122 @@
-    /*localStorage.removeItem('userData3');
-    localStorage.removeItem('usersCont');
-    localStorage.removeItem('sessionId');*/
+    // localStorage.removeItem('userData2');
+    // localStorage.removeItem('usersCont');
+    // localStorage.removeItem('sessionId');
+let correctEmail = (email) =>{
+    //validar el email
+    let dot = email.lastIndexOf('.');//localizamos el ultimo punto
+    let aroba = email.lastIndexOf('@');
+    if(dot > -1 && aroba> -1){
+        //si estan
+        let firstChars = email.substring(0,aroba);
+        let midChars = email.substring(aroba+1,dot);
+        let lastChars = email.substring(dot+1,email.length+1);
+        if(firstChars.length == 0 || midChars == 0 || lastChars == 0){
+            //si no hay caracteres en alguna
+            return false;
+        }else{
+            return true;
+        }
+    }else{
+        return false;
+    }
+}
+let getDate = () => {
+    let date = new Date();
 
+    let yyyy = date.getFullYear();
+    let mm = date.getMonth() + 1;
+    let dd = date.getDate();
+
+    formatDate = dd + '/' + mm + '/' + yyyy;
+    return formatDate;
+}
+
+let getTime = () =>{
+    let date = new Date() 
+    hh = date.getHours() 
+    mm = date.getMinutes() 
+    ss = date.getSeconds() 
+
+    dateUser = hh + ':' + mm + ':' + ss;
+    return dateUser
+}
 document.getElementById('userName').addEventListener('blur',function (){
+    //pierde el foco
     var value = this.value;
     if(value){
         //si hay texto en input
-        var userCont = parseInt( localStorage.getItem('usersCont'));
-        if(userCont){
-            let noExists = false;
-            //si hay usuarios
-            for (let i = 1; i <= userCont; i++) {
-                //recoremos todos los usuarios existentes
-                let idUser = 'userData' + i;//nombre de la variable
+        let emailC = correctEmail(value);//guardamos la respuyesta de si es correcto o no
+        console.log(emailC)
+        if(emailC){
+            //si es correcto
+            var userCont = parseInt( localStorage.getItem('usersCont'));
+            if(userCont){
+                let noExists = false;
+                //si hay usuarios
+                for (let i = 1; i <= userCont; i++) {
+                    //recoremos todos los usuarios existentes
+                    let idUser = 'userData' + i;//nombre de la variable
 
-                //obtenemos los datos de ese usuario
-                const savedUserData = JSON.parse(localStorage.getItem(idUser));
+                    //obtenemos los datos de ese usuario
+                    const savedUserData = JSON.parse(localStorage.getItem(idUser));
 
-                if(savedUserData['username'] === value){
-                    //si ya existe 
-                    localStorage.setItem('sessionId', savedUserData['id']);
-                    noExists = false;
-                    break;
-                }else{
-                    noExists = true;
+                    if(savedUserData['username'] === value){
+                        //si ya existe 
+                        localStorage.setItem('sessionId', savedUserData['id']);
+                        noExists = false;
+                        break;
+                    }else{
+                        noExists = true;
+                    }
                 }
-                //console.log(savedUserData['username']);
-            }
-            if(noExists){
-                //si no existe el usuario lo creamos
-                let idUser = 'userData' + (userCont+1);
+                if(noExists){
+                    //si no existe el usuario lo creamos
+                    let idUser = 'userData' + (userCont+1);
+                    //obtenemos la fecha actual
+                    let userDate = getDate();
+                    let userTime = getTime();
+                    let userData = {
+                        id: (userCont+1),
+                        usermail: value,
+                        day:userDate,
+                        time:userTime,
+                    };
+                    //creamos al usuario
+                    localStorage.setItem(idUser, JSON.stringify(userData));
+                    //establecemos que el usuario actual con el id
+                    localStorage.setItem('sessionId', (userCont+1));
+                    //sumamos el count de usuarios
+                    localStorage.setItem('usersCont',(userCont+1));
+                }
+            }else{
+                //si no hay ningun usuario
+
+                //iniciamos la variable contador de usuarios
+                localStorage.setItem('usersCont',1);
                 //obtenemos la fecha actual
                 let userDate = getDate();
                 let userTime = getTime();
+                
+                //creamos usuario con id 1 por ser el primero
                 let userData = {
-                    id: (userCont+1),
+                    id: 1,
                     usermail: value,
                     day:userDate,
                     time:userTime,
                 };
                 //creamos al usuario
-                localStorage.setItem(idUser, JSON.stringify(userData));
+                localStorage.setItem("userData1", JSON.stringify(userData));
                 //establecemos que el usuario actual con el id
-                localStorage.setItem('sessionId', (userCont+1));
-                //sumamos el count de usuarios
-                localStorage.setItem('usersCont',(userCont+1));
-                //console.log('sesion iniciada en usuario nuevo')
+                localStorage.setItem('sessionId', 1);
             }
-            //console.log('sesion iniciada en usuario existente')
+            //siempre redirige
+            window.location.href = '/pantalla2.html';
         }else{
-            //si no hay ningun usuario
-
-            //iniciamos la variable contador de usuarios
-            localStorage.setItem('usersCont',1);
-            //obtenemos la fecha actual
-            let userDate = getDate();
-            let userTime = getTime();
-            
-            //creamos usuario con id 1 por ser el primero
-            let userData = {
-                id: 1,
-                usermail: value,
-                day:userDate,
-                time:userTime,
-            };
-            //creamos al usuario
-            localStorage.setItem("userData1", JSON.stringify(userData));
-            //establecemos que el usuario actual con el id
-            localStorage.setItem('sessionId', 1);
+            //no es correcto
+            document.getElementById('emailAnswer').textContent = 'Correo incorrecto';
         }
-        //siempre redirige
-        window.location.href = '/pantalla2.html';
-
     }else{
         //si no hay texto en input
         console.log('vacio')
     }
 });
-
